a6618cb850098c1f5c482bbfb5e4e0aa17c8410a
wilsonzou <wilsonzou@tencent.com>
2021-10-24 18:54:53 +0800

    fix 项目

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# aa
+
 
 ## Project setup
 ```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>download-git-repo</h1>
+    <h2>wilsonzou</h2>
+    <h2>wilsonzou@tencent.com</h2>
+    <h2>15874191105</h2>
+    <h2>610043</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
